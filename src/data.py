0a3657ce73eec1179391d111de7f663c4f37c756
Madeleine Rischer <rischerm@hsu-hh.de>
2024-09-21 15:12:12 +0200

    Updated data.py
    dataframe handling

--- src/data.py
+++ src/data.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 def get_df():
     cols = ["name", "department", "position", "responsibilities", "email", "phone", "location", "description", "programs"]
-    return pd.read_csv("res/data.csv", sep=";", header=0, names=cols)
+    df = pd.read_csv("res/data.csv", sep=";", header=0, names=cols)
+
+    #   Replace NaN values with an empty string and convert everything to string
+    df = df.fillna('').astype(str)
+
+    return df
+
